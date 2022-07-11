33297510e8f1b50c91f40cf167ae612c931121a2
ramoeg04 <4494.geo@gmail.com>
2022-07-11 18:59:59 -0400

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'devups';
+  constructor(private metaTagService: Meta) {}
+  ngOnInit() {
+    this.metaTagService.addTags([
+      {
+        name: 'keywords',
+        content: 'D,devups,article,articulo,select, post, angular',
+      },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'author', content: 'Devups' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'date', content: '2019-10-31', scheme: 'YYYY-MM-DD' },
+      { charset: 'UTF-8' },
+    ]);
+  }
 }
